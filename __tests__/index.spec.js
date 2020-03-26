f29195cb292d8c34315eb50e169d5a5df0994c15
Michell Ayala <starships@outlook.com>
2020-03-26 14:44:28 -0600

    fix: typo, and replace .toEqual by .toBe

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -14,29 +14,29 @@ describe('isBalanced with only parenthesis', () => {
 
     it('should return false when the parenthesis are odd', () => {
       const result = isBalanced('()(')
-      expect(result),toEqual(false)
+      expect(result).toBe(false)
     })
 
     it('should return false when the parenthesis are not in order', () => {
       const result = isBalanced('))((')
-      expect(result).toEqual(false)
+      expect(result).toBe(false)
     })
   })
 
   describe.skip('with other characters', () => {
     it('should return true if the characters are even and nested', () => {
       const result = isBalanced('([{}])')
-      expect(result).toEqual(true)
+      expect(result).toBe(true)
     })
 
     it('should return true if the characters are not even', () => {
       const result = isBalanced('()[]{')
-      expect(result).toEqual(false)
+      expect(result).toBe(false)
     })
 
     it('should return false if the characters are not in order', () => {
       const result = isBalanced('()][{}')
-      expect(result).toEqual(false)
+      expect(result).toBe(false)
     })
   })
 })
